d8b7f73f03d99383ff0e5c11be6e73e91a7bf6db
rosamarjorie <rosa.marjoriev@gmail.com>
2025-11-04 14:01:43 -0300

    Update action_planilha.php

--- action_planilha.php
+++ action_planilha.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   
-$url = "https://script.google.com/macros/s/AKfycby3ZZKZf1a7Ee21j5d2f-eky6S7A7bWNLUoteP0t9oNgHTCzgDowQsfAcXEMTpApiOtNA/exec";
+$url = "https://script.google.com/macros/s/AKfycbyv5Qptu6dDO1EBNncIgK2mrdnrisBBn9hXlBFyv5_-PDaMFEcEmRxoChatQEkkc5RnJQ/exec";
  
   $data = [
     "nome" => $_POST["nome"] ?? "",
